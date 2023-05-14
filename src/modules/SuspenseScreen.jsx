bca078c01361f00c7e8ef653c13447078cb53e57
DanielBello7 <gokebello@gmail.com>
2023-05-14 14:40:35 +0100

    updates

--- src/modules/SuspenseScreen.jsx
+++ src/modules/SuspenseScreen.jsx
@@ -1,6 +1,6 @@
 function SuspenseScreen() {
     return (
-        <div className="w-100 d-flex align-items-center justify-content-center" style={{ minHeight: '600px' }}>
+        <div className="w-100 vh-100 d-flex align-items-center justify-content-center">
             <div className="spinner-grow spinner-grow-sm" role="status">
                 <span className="visually-hidden">Loading...</span>
             </div>
