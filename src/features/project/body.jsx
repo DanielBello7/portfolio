d56f40733db52f026b7c3b9f374e3cd4574f3072
DanielBello7 <gokebello@gmail.com>
2024-02-06 18:15:50 -0500

    chore: correct refactors

--- src/features/project/body.jsx
+++ src/features/project/body.jsx
@@ -1,86 +1,90 @@
 import { FaEye, FaGithub } from 'react-icons/fa';
 import { useModalData } from '@/modal-context.jsx';
 import { FormattedMessage } from 'react-intl';
-import { Fade } from 'react-awesome-reveal';
-import * as React from 'react';
 import { motion } from "framer-motion";
+import { container, item } from "./animation";
+import * as React from 'react';
 
 export default function Body(props) {
-  const { project } = props;
   const { ToggleToast } = useModalData();
+  const { project } = props;
 
   const tags = project?.tags.map((tag, index) => {
     return (
-      <span className='d-inline bg-light rounded-5' key={index}>
+      <motion.span className='d-inline bg-light rounded-5 p-1 px-3 me-2 mb-2'
+        key={index} variants={item}>
         {tag}
-      </span>
+      </motion.span>
     );
   });
 
   const onclick = () => {
-    project.repo && ToggleToast(true, 'Currently unavailable', false);
+    project.repo && ToggleToast(
+      true,
+      'Currently unavailable',
+      false
+    );
   }
 
   return (
     <div className='w-100 d-flex flex-column flex-sm-column flex-md-column align-items-center flex-lg-row my-5'>
       <div className='col-12 col-md-12 col-lg-4 d-flex flex-column'>
-        <div className='w-100' id="test">
-          <Fade className='d-inline bg-light px-3 py-1 rounded-5 me-2 mb-2' cascade triggerOnce>
-            {tags}
-          </Fade>
-        </div>
+        <motion.div id="test" className='w-100 flex-wrap' variants={container}
+          initial="hidden" animate="show" exit="hidden">
+          {tags}
+        </motion.div>
 
-        <Fade triggerOnce cascade>
-          <div className='w-100 mt-3 desc'>
+        <motion.div variants={container} initial="hidden" animate="show" exit="hidden">
+          <motion.div className='w-100 mt-3 desc' variants={item}>
             {project.desc}
-          </div>
-          <div className='w-100 mt-3 desc'>
+          </motion.div>
+          <motion.div className='w-100 mt-3 desc' variants={item}>
             {project.more}
-          </div>
-        </Fade>
+          </motion.div>
+        </motion.div>
 
-        <div className='w-100 mt-5'>
-          <Fade delay={600} triggerOnce>
+        <motion.div className='w-100 mt-5' initial={{ opacity: 0 }} animate={{ opacity: 1 }}
+          transition={{ delay: 0.6 }}>
+          <span>
+            <FormattedMessage id="projectRoleInfo" />
+          </span>
+          <span className='h3 bg-dark text-white txt'>
+            {project.role}
+          </span>
+        </motion.div>
+
+        <motion.div className='d-flex flex-row my-5' initial={{ opacity: 0 }} animate={{ opacity: 1 }}
+          transition={{ delay: 0.6 }}>
+          <a className='btn btn-dark px-4 d-flex flex-row align-items-center'
+            href={project.link} target="_blank" rel="noreferrer">
+            <FaEye size={15} className="me-2" />
             <span>
-              <FormattedMessage id="projectRoleInfo" />
+              <FormattedMessage id="projectsPreviewButton" />
             </span>
-            <span className='h3 bg-dark text-white txt'>
-              {project.role}
+          </a>
+          <a className='btn btn-outline-dark px-4 d-flex flex-row align-items-center ms-2'
+            rel="noreferrer" href={project.repo} target="_blank" onClick={onclick}>
+            <FaGithub size={15} className="me-2" />
+            <span>
+              <FormattedMessage id="projectsGithubButton" />
             </span>
-          </Fade>
-        </div>
-
-        <Fade triggerOnce delay={600}>
-          <div className='d-flex flex-row my-5'>
-            <a className='btn btn-dark px-4 d-flex flex-row align-items-center'
-              href={project.link} target="_blank" rel="noreferrer">
-              <FaEye size={15} className="me-2" />
-              <span>
-                <FormattedMessage id="projectsPreviewButton" />
-              </span>
-            </a>
-            <a className='btn btn-outline-dark px-4 d-flex flex-row align-items-center ms-2'
-              rel="noreferrer" href={project.repo} target="_blank" onClick={onclick}>
-              <FaGithub size={15} className="me-2" />
-              <span>
-                <FormattedMessage id="projectsGithubButton" />
-              </span>
-            </a>
-          </div>
-        </Fade>
+          </a>
+        </motion.div>
       </div>
 
       <div className='col-12 col-md-12 col-lg-8 d-flex flex-row justify-content-center justify-content-end-lg'>
-        <Fade className='col-12 col-md-12 col-lg-12 d-flex flex-row ms-0 ms-md-5 ms-lg-5' triggerOnce>
+        <motion.div className='col-12 col-md-12 col-lg-12 d-flex flex-row ms-0 ms-md-5 ms-lg-5'
+          initial={{ opacity: 0 }} animate={{ opacity: 1 }} viewport={{ once: true }}>
           <img
             src={project.img}
             id="proj-img"
             className='w-100 h-auto rounded-3 border shadow'
             alt="project-img"
           />
-        </Fade>
+        </motion.div>
       </div>
     </div>
   )
 }
+
 
--- src/features/project/gallery.jsx
+++ src/features/project/gallery.jsx
@@ -1,27 +1,31 @@
 import { FormattedMessage } from 'react-intl';
-import { Fade } from 'react-awesome-reveal';
+import { motion } from 'framer-motion';
+import { container, item } from "./animation";
 import * as React from 'react';
 
 export default function Gallery(props) {
   const { project } = props;
 
   const imgs = project.imgs?.map((img, index) => (
-    <div className='col-12 col-md-12 col-lg-12' key={index}>
+    <motion.div className='col-12 col-md-12 col-lg-6 mb-4' key={index}
+      variants={item}>
       <img src={img} className="more-img shadow rounded-3 border" alt="short-imgs" />
-    </div>
+    </motion.div>
   ));
 
   return (
     <div className='w-100 my-5'>
-      <Fade triggerOnce>
-        <div>
-          <h2 className='d-inline bg-dark text-white txt'>
-            <FormattedMessage id="projectsShotsMsg" />
-          </h2>
-        </div>
-      </Fade>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+        <h2 className='d-inline bg-dark text-white txt'>
+          <FormattedMessage id="projectsShotsMsg" />
+        </h2>
+      </motion.div>
+
       <div className='w-100 my-5 row m-0 p-0 g-4 d-flex flex-row align-items-center justify-content-between'>
-        <Fade className='col-12 col-md-6 col-lg-6' cascade triggerOnce>{imgs}</Fade>
+        <motion.div className='w-100 row col-12 col-md-6 col-lg-6' initial="hidden" whileInView="show"
+          exit="hidden" variants={container} viewport={{ once: true }}>
+          {imgs}
+        </motion.div>
       </div>
     </div>
   )
