ec09062f8392ba8679941869f13cd6c3559a1a10
DanielBello7 <gokebello@gmail.com>
2024-02-06 22:47:51 -0500

    chore: updates

---
+++ src/routes/default.routes.jsx
@@ -0,0 +1,29 @@
+import { FormattedMessage } from 'react-intl';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+export default function DefaultPage() {
+  return (
+    <div className="w-100 vh-100 d-flex flex-column justify-content-center align-items-center">
+      <h1 className="fw-bold" id="error">
+        <FormattedMessage id='notFoundErrorTitle' />
+      </h1>
+
+      <h1 className="col-5 text-center fw-bold" id="sorry">
+        <FormattedMessage id='notFoundErrorMsg' />
+      </h1>
+
+      <p className="sorry-txt">
+        <FormattedMessage id='notFoundErrorSupportTxt' />
+      </p>
+
+      <Link to="/" className="btn btn-dark d-flex align-items-center px-4">
+        <FaArrowLeft size={12} className="me-1" />
+        <span>
+          <FormattedMessage id='notFoundErrorButton' />
+        </span>
+      </Link>
+    </div>
+  )
+}
+
---
+++ src/routes/project.routes.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import Project from '@/features/project';
+
+export default function ProjectPage() {
+  return <Project />
+}
